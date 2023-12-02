21b501e96040df2142426f2dc6e38d81102efb68
SofiveBoy <152042268+SofiveBoy@users.noreply.github.com>
2023-12-02 12:30:37 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let name = document.getElementById("name")
+let Saynanmebtn = document.getElementById("Sayname")
+
+Saynanmebtn.addEventListener("click", () =>  {
+    name.innerHTML = "Hello แบมิง"
+})
+
